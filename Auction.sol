63daef5c163b76f0efa1c8aca232012689ac129c
sinqx <sinqxpls@gmail.com>
2023-02-06 14:52:55 +0600

    fuck

--- Auction.sol
+++ Auction.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Auction is ERC721{ 
 
