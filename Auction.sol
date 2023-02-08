42db40d4514cede29448803101d6bd78cfd2eb08
sinqx <sinqxpls@gmail.com>
2023-02-08 16:14:32 +0600

    placeBid update

--- Auction.sol
+++ Auction.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Auction is ERC721{ 
+contract Auction { 
 
     event Withdraw(address indexed reciever, uint256 amount);
     event NewBid(uint256 nftId, address bidder, uint256 amount);
     event FinishAuction(address buyer, address seller, uint256 finalPrice, uint256 nftId);
    
-    
+    uint32 private auctionCounter;
     mapping(uint256 => bool) activeNFT;
-    mapping(bytes32 => uint256) highestBid;
-    mapping(bytes32 => address) highestBidder;
-    mapping(bytes32 => AuctionInfo) auctions;
+    mapping(uint32 => uint256) highestBid;
+    mapping(uint32 => address) highestBidder;
+    mapping(uint32 => AuctionInfo) auctions;
     mapping(address => mapping(uint32 => bool)) NFTsOwner;
-    mapping(bytes32 => mapping(address => uint256)) bids;
+    mapping(uint32 => mapping(address => uint256)) bids;
     mapping(uint32 => mapping(string => string)) NFTs;
 
     struct AuctionInfo {
@@ -28,21 +28,27 @@ contract Auction is ERC721{
         address nftOwner;
     }
 
+    mapping(address => bool) internal locked;
+
+    modifier noReentrant(address _address) {
+        require(!locked[_address], "No re-entrancy");
+        locked[_address] = true;
+        _;
+        locked[_address] = false;
+    }
+
     function checkContractBalance() public view returns(uint256) {
         return address(this).balance;
     }
 
-    // function ownerOf(uint256 nftId) external view returns (address) {
-    //     return _owners[tokenId];
-    // }
+    function createAuction(uint256 _price, uint32 _nftId, uint8 _auctionTime) external noReentrant(msg.sender) {
+        require(msg.sender != address(0));  
+        require(NFTsOwner[msg.sender][_nftId]); 
+        require(!activeNFT[_nftId]);    
+        require(_price > 0);        
 
-    function createAuction(uint256 _price, uint32 _nftId, uint8 _auctionTime) external {
-        require(msg.sender != address(0));
-        require(NFTsOwner[msg.sender][_nftId]);
-        require(!activeNFT[_nftId]);
-        require(_price > 0);
-        
-        auctions[keccak256(abi.encodePacked(_nftId, block.timestamp))] = 
+        unchecked { auctionCounter++; }
+        auctions[ auctionCounter ] =       ////////////////////
         AuctionInfo (
             true,
             _nftId,
@@ -54,64 +60,72 @@ contract Auction is ERC721{
         activeNFT[_nftId] = true;
     }
 
-    function callBackNFT(bytes32 _auctionAddress) external {
-        AuctionInfo memory _auction = auctions[_auctionAddress];
+    function callBackNFT(uint32 _auctionId) external {
+        AuctionInfo memory _auction = auctions[_auctionId];
+
         require(msg.sender == _auction.nftOwner);
         require(block.timestamp > _auction.endTime);
         require(!_auction.progress);
 
         NFTsOwner[msg.sender][_auction.nftId] = true;
         delete activeNFT[_auction.nftId];
-        delete auctions[_auctionAddress];
+        delete auctions[_auctionId];
     }
 
-    function setMinBid(bytes32 _auctionAddress, uint256 _minBid) external {
-        require(auctions[_auctionAddress].nftOwner == msg.sender);
-        auctions[_auctionAddress].minBid = _minBid;
+    function setMinBid(uint32 _auctionId, uint256 _minBid) external {
+        require(auctions[_auctionId].nftOwner == msg.sender);
+        auctions[_auctionId].minBid = _minBid;
     }
 
-    function placeBid(uint _nftId, bytes32 _auctionAddress, uint256 _minBid) external payable {   
-        AuctionInfo memory _auction = auctions[_auctionAddress];
+    function placeBid(uint32 _nftId, uint32 _auctionId) external payable {   
+        AuctionInfo memory _auction = auctions[_auctionId];
         
         require(block.timestamp < _auction.endTime);
-        require(msg.value > highestBid[_auctionAddress] + _auction.minBid);
+        require(msg.sender.balance > highestBid[_auctionId] + _auction.minBid);
 
-        payable(address(this)).transfer(msg.value);
-        payable(address(highestBidder[_auctionAddress])).transfer(highestBid[_auctionAddress]);
+        //address(this).transfer(msg.value);
+        //payable(address(highestBidder[_auctionId])).transfer(highestBid[_auctionId]);
       
-        highestBid[_auctionAddress] += msg.value;
-        highestBidder[_auctionAddress] = msg.sender;
-        bids[_auctionAddress][msg.sender] += msg.value;
+        highestBid[_auctionId] += msg.value;
+        highestBidder[_auctionId] = msg.sender;
+        bids[_auctionId][msg.sender] += msg.value;
 
         emit NewBid(_nftId, msg.sender, msg.value);
     }
 
-    function endAuction(bytes32 _auctionAddress) external {
-        AuctionInfo memory _auction = auctions[_auctionAddress];
-
+    function endAuction(uint32 _auctionId) external noReentrant(msg.sender) {
+        AuctionInfo memory _auction = auctions[_auctionId];
+        require(_auction.nftOwner == msg.sender);
         require(_auction.endTime > block.timestamp);
         require(_auction.progress);
-
-        payable(address(_auction.nftOwner)).transfer(highestBid[_auctionAddress]);
-
+   //     (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        payable(address(_auction.nftOwner)).transfer(highestBid[_auctionId]);
+        delete highestBid[_auctionId];
         _auction.progress = false;
+        ////////////////////////////////
     }
 
-     function withDraw(bytes32 _auctionAddress) external {
-        require(msg.sender != highestBidder[_auctionAddress]);
-        require(bids[_auctionAddress][msg.sender] > 0);
+     function withDraw(uint32 _auctionId) external noReentrant(msg.sender) {  ////////////////////////////
+        require(msg.sender != highestBidder[_auctionId]);
+        // require(highestBidder[_auctionAddress] != address(0));
+        require(bids[_auctionId][msg.sender] > 0);
 
-        payable(msg.sender).transfer(bids[_auctionAddress][msg.sender]);
+        payable(msg.sender).transfer(bids[_auctionId][msg.sender]);
 
-        delete bids[_auctionAddress][msg.sender];
+        delete bids[_auctionId][msg.sender];
+        delete highestBidder[_auctionId];
     }
 
     function mint(string memory _tokenURI, string memory _name, string memory _symbol) external {
-        require(_symbol.leangth < 6);
-        require(_symbol.leangth > 2);
-        require(_name.leangth < 16);
-         require(_name.leangth > 16);
 
+
+
+
+
+        // require(_symbol.leangth < 6);
+        // require(_symbol.leangth > 2);
+        // require(_name.leangth < 16);
+        // require(_name.leangth > 16);
         
         // nftIds += nftIds;
         // ERC721.price[_tokenId] = _price;
